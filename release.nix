f61281a0758e631aae101983c79ef042dfe6a5fb
Karim-Al-Shammas <kha24@mail.aub.edu>
2025-07-29 18:35:01 +0300

    small changes

--- release.nix
+++ release.nix
@@ -20,12 +20,6 @@ let
     pip2nix = filterAttrs (n: v: n != "recurseForDerivations") (
       pkgs.recurseIntoAttrs (
         builtins.listToAttrs (map make-pip2nix ([]
-        ++ optional (hasAttr "python27Packages" pkgs) {pythonVersion = "27";}
-        ++ optional (hasAttr "python33Packages" pkgs) {pythonVersion = "33";}
-        ++ optional (hasAttr "python34Packages" pkgs) {pythonVersion = "34";}
-        ++ optional (hasAttr "python35Packages" pkgs) {pythonVersion = "35";}
-        ++ optional (hasAttr "python36Packages" pkgs) {pythonVersion = "36";}
-        ++ optional (hasAttr "python37Packages" pkgs) {pythonVersion = "37";}
         ++ optional (hasAttr "python38Packages" pkgs) {pythonVersion = "38";}
         ++ optional (hasAttr "python39Packages" pkgs) {pythonVersion = "39";}
         ++ optional (hasAttr "python310Packages" pkgs) {pythonVersion = "310";}
@@ -36,9 +30,9 @@ let
 
     docs = pkgs.stdenv.mkDerivation {
       name = "pip2nix-docs";
-      src = pip2nix.python36.src;
+      src = pip2nix.python311.src;
       #outputs = [ "html" ];  # TODO: PDF would be even nicer on CI
-      buildInputs = [ pip2nix.python36 ] ++ (with  pkgs.python36Packages; [
+      buildInputs = [ pip2nix.python311 ] ++ (with  pkgs.python311Packages; [
         sphinx
       ]);
       buildPhase = ''
